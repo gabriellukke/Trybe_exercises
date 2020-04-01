2b609fb14be356405fe10c72dac4f4cb28ac52af
gabriellukke <gabriellukke@outlook.com>
2020-04-01 11:22:19 -0300

    adjust the for loop

--- exercises/4_2/ex04.js
+++ exercises/4_2/ex04.js
@@ -1,6 +1,6 @@
 n = 5;
 
-for (let i = 1; i <= n; i = i + 2) {
+for (let i = 1; i <= n; i += 2) {
   let space = " ".repeat((n - i) / 2);
   let star = "*".repeat(i);
   console.log(space + star + space); 
