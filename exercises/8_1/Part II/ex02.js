d5474623a6ccf2f0e155dc0ed71420b73080e3c6
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-05-06 18:58:55 -0300

    Exercise 3 of Part II done

---
+++ exercises/8_1/Part II/ex02.js
@@ -0,0 +1,12 @@
+const longestWord = (phrase) => { 
+  const arr = phrase.split(' ');
+  let word = arr[0];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (word.length < arr[i].length) {
+      word = arr[i];
+    };
+  };
+  return word;
+}
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
