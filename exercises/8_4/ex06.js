94c85655e10e4e2d1fde975bb6da6ae35c074ec3
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-05-09 17:19:40 -0300

    Exercise 6 done

---
+++ exercises/8_4/ex06.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+
+const removeVowels = (word) => {
+  const characters = word.split('');
+  let results = '';
+  let number = 0;
+
+  for (let i = 0; i < characters.length; i += 1) {
+    if (
+      characters[i] === 'a' ||
+      characters[i] === 'o' ||
+      characters[i] === 'i' ||
+      characters[i] === 'e' ||
+      characters[i] === 'u'
+    ) {
+      number += 1;
+      results += number;
+    } else {
+      results += characters[i];
+    }
+  }
+  return results;
+};
+
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+/*
+  Use a variável parameter como parâmetro da função acima, escreva testes
+  para verificar se a mesma está retornando a como se vê na variável result
+  e, caso não esteja, altere o código para que ele passe nos testes.
+  Lembre-se: testes pequenos e numerosos! Escreva um por vez e vá corrigindo
+  a função aos poucos:
+*/
+assert.strictEqual(typeof(removeVowels), 'function');
+assert.equal(removeVowels(parameter), result);
