255feb3ca9746af9c6ed8bd56131b4db75828927
gabriellukke <gabriellukke@outlook.com>
2020-05-19 12:57:29 -0300

    Function and Test 1 done

---
+++ exercises/10_1/tests/ex01.test.js
@@ -0,0 +1,24 @@
+const sum = require('../src/ex01');
+
+describe('sum', () => {
+
+  test('Add two numbers', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Throws a error when a string is passed as argument', () => {
+    expect(() => {
+      sum(4, '5')
+    })
+    .toThrow(Error);
+  });
+
+  test('Error mensage is “parameters must be numbers”', () => {
+    expect(() => {
+      sum(4, '5')
+    })
+    .toThrow(/parameters must be numbers/)
+  });
+
+})
