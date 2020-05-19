56ab207aacb757567342b8c9de9b1b51815debea
gabriellukke <gabriellukke@outlook.com>
2020-05-19 15:44:39 -0300

    Function and Test 5 done

---
+++ exercises/10_1/tests/ex05.test.js
@@ -0,0 +1,20 @@
+const myRemoveWithoutCopy = require('../src/ex05');
+
+describe('myRemoveWithoutCopy', () => {
+
+  test('Return the array with the item removed', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  test('Change the array passed as argument', () => {
+    const array = [1, 2, 3, 4];
+    myRemoveWithoutCopy(array, 2);
+    expect(array).toEqual([1, 3, 4]);
+  });
+
+  test("Return the same array with no changes if the item doesn't contains in the array", () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+  });
+
+});
