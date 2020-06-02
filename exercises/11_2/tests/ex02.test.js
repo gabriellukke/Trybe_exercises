cf85d70395375f87187a62a3b4bc3d4c2d6774c9
gabriellukke <gabriellukke@outlook.com>
2020-06-02 16:47:28 -0300

    Exercise 2 done

--- exercises/11_2/tests/ex02.test.js
+++ exercises/11_2/tests/ex02.test.js
@@ -3,9 +3,10 @@ let randomNumber = () => parseInt(Math.random() * (100 - 0));
 test('randomNumber', () => {
   randomNumber = jest
     .fn()
-    .mockReturnValue(10);
-
-  expect(randomNumber()).toBe(10);
+    .mockImplementationOnce((a, b) => a / b);
+    
+  expect(randomNumber(10, 2)).toBe(5);
   expect(randomNumber).toHaveBeenCalled();
   expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber).toHaveBeenCalledWith(10, 2);
 });
