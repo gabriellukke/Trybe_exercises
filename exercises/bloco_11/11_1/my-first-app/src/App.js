2e1536fca18e5a8b8450d70dfc91a08a359677d1
gabriellukke <gabriellukke@outlook.com>
2020-06-03 02:27:48 -0300

    Created task list

--- exercises/bloco_11/11_1/my-first-app/src/App.js
+++ exercises/bloco_11/11_1/my-first-app/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const tasks = ['Programar', 'Codar', 'Criar códigos', 'Estudar React', 'Estudar JS']
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ul>
+     { tasks.map(task => <li> {task} </li>) }
+    </ul> 
+   )
+  }
 
 export default App;
