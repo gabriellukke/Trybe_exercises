9295131985a63d5b429a22c248ab97e39fa43af3
gabriellukke <gabriellukke@outlook.com>
2020-06-10 17:31:57 -0300

    First Requirement done

--- exercises/bloco_12/12_2/my-form-2.0/src/App.js
+++ exercises/bloco_12/12_2/my-form-2.0/src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const states = ['Rio de Janeiro', 'Minas Gerais', 'São Paulo', 'Amapá', 'Amazonas', 'Alagoas', 'Pernambuco', 'Bahia', 'Distrito Federal', 'Paraíba', 'Piauí', 'Rio Grande do Norte', 'Tocantins', 'Mato Grosso', 'Mato Grosso do Sul', 'Roraima', 'Espirito Santo', 'Rondônia', 'Sergipe', 'Pará', 'Ceará', 'Santa Catarina', 'Paraná', 'Acre', 'Maranhão', 'Goiás', 'Rio Grande do Sul'];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      cpf: '',
+      address: '',
+      city: '',
+      countryState: '',
+      addressType: {
+        house: false,
+        apartament: false,
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form>
+          <fieldset>
+            <legend>Dados Pessoais</legend>
+            <div className="container">
+              Nome:
+              <input 
+              type="name"
+              name="name"
+              maxLength="40"
+              required
+              value={this.state.name}
+              />
+            </div>
+            <div className="container">
+              Email:
+              <input
+              type="email"
+              name="email"
+              maxLength="50"
+              required
+              value={this.state.email}
+              />
+            </div>
+            <div className="container">
+              CPF:
+              <input
+              type="text"
+              name="cpf"
+              maxLength="11"
+              required
+              value={this.state.cpf}
+              />
+            </div>
+            <div className="container">
+              Endereço
+              <input
+              type="text"
+              name="address"
+              maxLength="200"
+              required
+              value={this.state.address}
+              />
+            </div>
+            <div className="container">
+              Cidade:
+              <input
+              type="text"
+              name="city"
+              maxLength="28"
+              value={this.state.city}
+              />
+            </div>
+            <div className="container">
+              Estado:
+              <select
+              type="text"
+              name="countryState"
+              required
+              value={this.state.countryState}
+              />
+              {states.sort().map((value, key) => <option key={key}>{value}</option>)}
+            </div>
+            <div>
+              Tipo:
+              <label>
+                <input
+                type="radio"
+                name="type"
+                value={this.state.addressType.house}
+                />
+                Casa
+              </label>
+              <label>
+                <input
+                type="radio"
+                name="type"
+                value={this.state.addressType.apartament}
+                />
+                Apartamento
+              </label>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
