b32ba5514dcbe514d3b39f3f6c83b31e714c3847
gabriellukke <gabriellukke@outlook.com>
2020-09-21 15:57:38 -0300

    Ex02

--- exercises/bloco_26/26_1/index.js
+++ exercises/bloco_26/26_1/index.js
@@ -6,7 +6,35 @@ const calcIMC = () => {
 
   const imc = (weight / Math.pow(height, 2)).toFixed(2);
 
-  console.log('Your IMC is:', imc);
+  console.log(imc, 'WTF ?')
+
+  if (imc < 18.5) {
+    console.log(`Your IMC is: ${imc}, You're underweight(thinness)`);
+    return;
+  };
+
+  if (imc >= 18.5 && imc < 25) {
+    console.log(`Your IMC is: ${imc}, You're with normal weight`);
+    return;
+  };
+
+  if (imc >= 25 && imc < 30) {
+    console.log(`Your IMC is: ${imc}, You're overweight (overweight)`);
+    return;
+  };
+
+  if (imc >= 30 && imc < 35) {
+    console.log(`Your IMC is: ${imc}, You're with obesity grade I`);
+    return;
+  };
+  
+  if (imc >= 35 && imc < 40) {
+    console.log(`Your IMC is: ${imc}, You're with obesity grade II`);
+    return;
+  };
+
+  console.log(`Your IMC is: ${imc}, You're with obesity grade III and IV`);
+
 };
 
 calcIMC();
